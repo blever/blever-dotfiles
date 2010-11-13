500e7c2f23ba1c62af96f1123d546439bc31bb1f
Ben Lever <ben.lever@gmail.com>
2010-11-13 14:00:36 +1100

    Fix typo in install script..

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DOTFILES=$HOME/local/dotfiles
 
 # Install bash scripts
 ln -sf $DOTFILES/bash/.bashrc        $HOME/.bashrc
-ln -sf $DOTFILES/bash/.bash_proflie  $HOME/.bash_profile
+ln -sf $DOTFILES/bash/.bash_profile  $HOME/.bash_profile
 
 # Install vim scripts
 ln -sf $DOTFILES/vim/.vimrc          $HOME/.vimrc
