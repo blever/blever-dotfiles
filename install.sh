679416035a836e1e2bafd1eb01dc6d70cf7c411d
Ben Lever <ben.lever@gmail.com>
2010-11-16 09:26:04 +1100

    Remove '.' from bashrc and bash_profile.
    The '.' at the front of the filenames is a bit annoying and not
    really necessary given their symlink'd to.

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 DOTFILES=$HOME/local/dotfiles
 
 # Install bash scripts
-ln -sf $DOTFILES/bash/.bashrc        $HOME/.bashrc
-ln -sf $DOTFILES/bash/.bash_profile  $HOME/.bash_profile
+ln -sf $DOTFILES/bash/bashrc         $HOME/.bashrc
+ln -sf $DOTFILES/bash/bash_profile   $HOME/.bash_profile
 
 # Install vim scripts
 ln -sf $DOTFILES/vim/.vimrc          $HOME/.vimrc
